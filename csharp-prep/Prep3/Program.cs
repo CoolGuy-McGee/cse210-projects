aeef3f39321d382e67b1852a1be2ed6135ed44e2
CoolGuy-McGee <daltonbroderson@outlook.com>
2025-04-28 18:01:29 -0700

    Completed Prep 3 assignment with the two stretch goals:
    Keep track of how many guesses the user has made and inform them of it at the end of the game.
    
    After the game is over, ask the user if they want to play again. Then, loop back and play the whole game again and continue this loop as long as they keep saying "yes".

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = randomGenerator.Next(1, 101);
+        int guessCount = 0;
+        string answer = "yes";
+        
+        while (answer == "yes")
+        {
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount ++;
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($" \nYou guessed it!\nNumber of guesses: {guessCount}\n");
+                    Console.Write("Do you want to play again? ");
+                    answer = Console.ReadLine();
+                    if (answer == "yes")
+                    {
+                        number = randomGenerator.Next(1, 101);
+                        guess = randomGenerator.Next(1, 101);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thank you for playing!");
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
